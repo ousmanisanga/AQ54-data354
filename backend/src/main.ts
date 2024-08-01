48ab19620b8276ce9177bac1217ada0770a9e70a
ousmanisanga <ousmanisanga3@gmail.com>
2024-08-01 11:29:02 +0000

    third update of backend and frontend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+  .setTitle('AQ54 API')
+  .setDescription('REST API for authentication , users and sensor data')
+  .setVersion('1.0')
+  .addTag('Users')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document)
+  
   // Configure CORS
   app.enableCors({
     origin: 'http://localhost:3001', 
